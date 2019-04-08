aebb4e86c1bea611c50c92fa77dfb973e7063dbc
HazemHamdeni <47541287+HazemHamdeni@users.noreply.github.com>
2019-04-08 14:20:54 +0200

    Add files via upload

--- dep.c
+++ dep.c
@@ -2,24 +2,28 @@
 #include <stdio.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
-//#include "scrolling.c"
 
 
-int dep(void){
+
+
+int main(void){
 	SDL_Surface *screen;
 	SDL_Surface *image;
+	SDL_Surface *masque;
    	SDL_Surface *perso;
 	SDL_Event event;
 	SDL_Rect positionecran;
 	SDL_Rect posp;
-	SDL_Rect posobstacle1;
-
+	
+	
 	char pause;
+	int collisionRightSide=0;
+	
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
 		printf("Unable to initialize SDL: %s\n", SDL_GetError());
 		return -1;
 	}
-	screen = SDL_SetVideoMode(1920, 1080, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	screen = SDL_SetVideoMode(8000, 1024, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 	if(screen == NULL){
 		printf("Unable to set video mode: %s\n", SDL_GetError());
 		return -1;
@@ -29,6 +33,7 @@ int dep(void){
 		printf("Unable to load bitmap: %s\n", SDL_GetError());
 		return 1;
 	}
+	masque = IMG_Load("backgroundMasque.png");
 	
 	positionecran.x = 0;
 	positionecran.y = 0;
@@ -44,19 +49,24 @@ int dep(void){
 	int done = 1;
 	SDL_EnableKeyRepeat(10, 10);
 	while(done){
+		SDL_BlitSurface(masque,NULL,screen,&positionecran);
 		SDL_BlitSurface(image,NULL,screen,&positionecran);
 		SDL_BlitSurface(perso,NULL,screen, &posp);
                 SDL_Flip(screen);
 		
 		while(SDL_PollEvent(&event)){
-			//check for messages
+			
 			switch(event.type){
 				case SDL_QUIT:
 					done = 0;
-					break;	
+					break;
+			
+           
 				case SDL_MOUSEBUTTONUP:
             					posp.x = event.button.x; /* On change les coordonnées de seif */
             		                        posp.y = event.button.y;
+					        if (event.button.button == SDL_BUTTON_RIGHT) /* On arrête le programme si on a fait un clic droit */
+							done = 0;
             				break;
 			 
 				case SDL_KEYDOWN:	
@@ -68,23 +78,22 @@ int dep(void){
 							break;
 						case SDLK_LEFT:
 							posp.x -= 20;
-							//positionecran.x=scrollToLeft(positionecran.x);
-							positionecran.x+=10;
-							SDL_BlitSurface(image,NULL,screen,&positionecran);
-							SDL_UpdateRect(screen, 0, 0, 0, 0);
 							break;
 						case SDLK_DOWN:
 							posp.y += 20;
+		 			SDL_BlitSurface(image,NULL,screen,&positionecran);
+					posp.y=790;
+		
 							break;
+		
 						case SDLK_UP:
 							posp.y -= 20;
+			
 							break;
 						case SDLK_RIGHT:
 							posp.x += 20;
-							//positionecran.x=scrollToRight(positionecran.x);
-							positionecran.x-=10;
-							SDL_BlitSurface(image,NULL,screen,&positionecran);
-							SDL_UpdateRect(screen, 0, 0, 0, 0);
+					
+				
 							break;
 					}
 				}
